0f2df42aaed3b7f0a1f1a87eacbb7cbeb8bf3d5c
Unknown <jonathandao0@gmail.com>
2022-09-17 14:29:14 -0700

    Code Reorganization

---
+++ edgeTest/apriltag_pipeline.py
@@ -0,0 +1,121 @@
+
+import depthai as dai
+
+
+def create_pipeline_rgb():
+    # Create pipeline
+    pipeline = dai.Pipeline()
+
+    # Define sources and outputs
+    cam = pipeline.create(dai.node.ColorCamera)
+    manip = pipeline.create(dai.node.ImageManip)
+    edgeDetector = pipeline.create(dai.node.EdgeDetector)
+
+    xoutRgb = pipeline.create(dai.node.XLinkOut)
+    xoutEdge = pipeline.create(dai.node.XLinkOut)
+    xinEdgeCfg = pipeline.create(dai.node.XLinkIn)
+
+    videoStr = "rgb"
+    edgeStr = "rgb edge"
+    edgeCfgStr = "edge cfg"
+
+    xoutRgb.setStreamName(videoStr)
+    xoutEdge.setStreamName(edgeStr)
+    xinEdgeCfg.setStreamName(edgeCfgStr)
+
+    # Properties
+    cam.setBoardSocket(dai.CameraBoardSocket.RGB)
+    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    cam.setPreviewSize(1920, 1080)
+    # camRgb.setFps(60) # Breaks edge detection
+
+    manip.initialConfig.setResize(cam.getVideoWidth(), cam.getVideoHeight())
+    manip.initialConfig.setFrameType(dai.ImgFrame.Type.GRAY8)
+    manip.setMaxOutputFrameSize(cam.getVideoWidth() * cam.getVideoHeight())
+
+    edgeDetector.setMaxOutputFrameSize(cam.getVideoWidth() * cam.getVideoHeight())
+
+    # Linking
+    cam.video.link(manip.inputImage)
+    manip.out.link(edgeDetector.inputImage)
+    manip.out.link(xoutRgb.input)
+
+    edgeDetector.outputImage.link(xoutEdge.input)
+
+    xinEdgeCfg.out.link(edgeDetector.inputConfig)
+
+    pipeline_info = {
+        'resolution_x': cam.getVideoWidth(),
+        'resolution_y': cam.getVideoHeight(),
+        'videoQueue': videoStr,
+        'edgeQueue': edgeStr,
+        'edgeCfgQueue': edgeCfgStr
+    }
+
+    # OAK-D Lite @ 1080P
+    camera_params = {
+        "fx": 0.00337,
+        "fY": 0.00337,
+        "cx": int(cam.getVideoWidth() / 2),
+        "cy": int(cam.getVideoHeight() / 2)
+    }
+
+    return pipeline, pipeline_info, camera_params
+
+
+def create_pipeline_mono():
+    pipeline = dai.Pipeline()
+
+    # Define sources and outputs
+    cam = pipeline.create(dai.node.MonoCamera)
+
+    edgeDetector = pipeline.create(dai.node.EdgeDetector)
+
+    xoutMono = pipeline.create(dai.node.XLinkOut)
+    xoutEdge = pipeline.create(dai.node.XLinkOut)
+    xinEdgeCfg = pipeline.create(dai.node.XLinkIn)
+
+    videoStr = "mono"
+    edgeStr = "mono left"
+    edgeCfgStr = "edge cfg"
+
+    xoutMono.setStreamName(videoStr)
+    xoutEdge.setStreamName(edgeStr)
+    xinEdgeCfg.setStreamName(edgeCfgStr)
+
+    # Properties
+    cam.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
+    cam.setBoardSocket(dai.CameraBoardSocket.LEFT)
+
+    # Linking
+    cam.out.link(xoutMono.input)
+    cam.out.link(edgeDetector.inputImage)
+
+    edgeDetector.outputImage.link(xoutEdge.input)
+
+    xinEdgeCfg.out.link(edgeDetector.inputConfig)
+
+    pipeline_info = {
+        'resolution_x': cam.getVideoWidth(),
+        'resolution_y': cam.getVideoHeight(),
+        'videoQueue': videoStr,
+        'edgeQueue': edgeStr,
+        'edgeCfgQueue': edgeCfgStr
+    }
+
+    # # OAK-D Lite @ 480P
+    # camera_params = {
+    #     "fx": 0.00337,
+    #     "fY": 0.00337,
+    #     "cx": int(cam.getVideoWidth() / 2),
+    #     "cy": int(cam.getVideoHeight() / 2)
+    # }
+    # OAK-D Lite @ 800P
+    camera_params = {
+        "fx": 0.00337 * 1000,
+        "fY": 0.00337 * 1000,
+        "cx": int(cam.getVideoWidth() / 2),
+        "cy": int(cam.getVideoHeight() / 2)
+    }
+
+    return pipeline, pipeline_info, camera_params
--- edgeTest/main.py
+++ edgeTest/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import argparse
+import logging
 
 import cv2
 import depthai as dai
@@ -9,210 +11,160 @@ from pupil_apriltags import Detector
 import constants
 
 from common.utils import FPSHandler
+from edgeTest import apriltag_pipeline
 
-# Create pipeline
-pipeline = dai.Pipeline()
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', dest='debug', action="store_true", default=False, help='Start in Debug Mode')
+parser.add_argument('-c', dest='camera_type', action="store", type=str, default='RGB', help='Set camera type '
+                                                                                            '(RGB, MONO. Default: RGB)')
 
-CAMERA_TYPE = 'RGB'
+args = parser.parse_args()
+
+log = logging.getLogger(__name__)
 
 
-if CAMERA_TYPE == 'RGB':
-    # Define sources and outputs
-    cam = pipeline.create(dai.node.ColorCamera)
+def main():
+    log.info("Starting AprilTag Detector")
+    CAMERA_TYPE = args.camera_type
 
-    edgeDetector = pipeline.create(dai.node.EdgeDetector)
-    xoutRgb = pipeline.create(dai.node.XLinkOut)
-    xoutEdge = pipeline.create(dai.node.XLinkOut)
-    xinEdgeCfg = pipeline.create(dai.node.XLinkIn)
+    if CAMERA_TYPE.upper() is not 'RGB' or CAMERA_TYPE.upper() is not 'MONO':
+        log.error("CAMERA_TYPE not recognized \"{}\". Defaulting to RGB".format(CAMERA_TYPE))
 
-    videoStr = "rgb"
-    edgeStr = "rgb edge"
-    edgeCfgStr = "edge cfg"
+    if CAMERA_TYPE == 'MONO':
+        pipeline, pipeline_info, camera_params = apriltag_pipeline.create_pipeline_mono()
+    else:
+        pipeline, pipeline_info, camera_params = apriltag_pipeline.create_pipeline_rgb()
 
-    xoutRgb.setStreamName(videoStr)
-    xoutEdge.setStreamName(edgeStr)
-    xinEdgeCfg.setStreamName(edgeCfgStr)
+    detector = Detector(families='tag36h11',
+                        nthreads=4,
+                        quad_decimate=1.0,
+                        quad_sigma=0.0,
+                        refine_edges=1,
+                        decode_sharpening=0.25,
+                        debug=0)
 
-    # Properties
-    cam.setBoardSocket(dai.CameraBoardSocket.RGB)
-    cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    # camRgb.setFps(60) # Breaks edge detection
+    fps = FPSHandler()
 
-    edgeDetector.setMaxOutputFrameSize(cam.getVideoWidth() * cam.getVideoHeight())
+    # Connect to device and start pipeline
+    with dai.Device(pipeline) as device:
+        # Output/input queues
+        videoQueue = device.getOutputQueue(pipeline_info['videoQueue'], 1, False)
+        edgeQueue = device.getOutputQueue(pipeline_info['edgeQueue'], 1, False)
+        edgeCfgQueue = device.getInputQueue(pipeline_info['edgeCfgQueue'])
 
-    # Linking
-    cam.video.link(xoutRgb.input)
-    cam.video.link(edgeDetector.inputImage)
+        print("Switch between sobel filter kernels using keys '1' and '2'")
 
-    edgeDetector.outputImage.link(xoutEdge.input)
+        while True:
+            videoOutput = videoQueue.get()
+            edgeOutput = edgeQueue.get()
 
-    xinEdgeCfg.out.link(edgeDetector.inputConfig)
-else:
-    # Define sources and outputs
-    cam = pipeline.create(dai.node.MonoCamera)
+            videoFrame = videoOutput.getFrame()
+            edgeFrame = edgeOutput.getFrame()
 
-    edgeDetector = pipeline.create(dai.node.EdgeDetector)
+            thresh = cv2.threshold(edgeFrame, 25, 255, cv2.THRESH_BINARY)[1]
+            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    xoutMono = pipeline.create(dai.node.XLinkOut)
-    xoutEdge = pipeline.create(dai.node.XLinkOut)
-    xinEdgeCfg = pipeline.create(dai.node.XLinkIn)
+            squares = []
+            contourInfo = []
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
 
-    videoStr = "mono"
-    edgeStr = "mono left"
-    edgeCfgStr = "edge cfg"
+                # TODO: Figure out better area value
+                if len(cnt) == 4 and area > 1000:
+                    squares.append(cnt)
 
-    xoutMono.setStreamName(videoStr)
-    xoutEdge.setStreamName(edgeStr)
-    xinEdgeCfg.setStreamName(edgeCfgStr)
+                    # Create a bounding box around the square with padding and save it for later
+                    rect = cv2.minAreaRect(cnt)
+                    points = cv2.boxPoints(rect)
+                    xmin = min(x for x in points[:, 0])
+                    xmax = max(x for x in points[:, 0])
+                    ymin = min(y for y in points[:, 1])
+                    ymax = max(y for y in points[:, 1])
+                    xmin = max(int(xmin - xmin * constants.PADDING_PERCENTAGE), 0)
+                    xmax = min(int(xmax + xmin * constants.PADDING_PERCENTAGE), videoFrame.shape[1])
+                    ymin = max(int(ymin - ymin * constants.PADDING_PERCENTAGE), 0)
+                    ymax = min(int(ymax + ymax * constants.PADDING_PERCENTAGE), videoFrame.shape[0])
 
-    # Properties
-    cam.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-    cam.setBoardSocket(dai.CameraBoardSocket.LEFT)
+                    contourData = {'Contour': cnt,
+                                   'x_min': xmin,
+                                   'x_max': xmax,
+                                   'y_min': ymin,
+                                   'y_max': ymax,
+                                   'area': area}
 
-    # Linking
-    cam.out.link(xoutMono.input)
-    cam.out.link(edgeDetector.inputImage)
+                    contourInfo.append(contourData)
 
-    edgeDetector.outputImage.link(xoutEdge.input)
+            contourInfo = sorted(contourInfo, key=lambda d: d['area'], reverse=True)
+            count = 0
+            positives = 0
+            for contour in contourInfo:
+                frameSegment = videoFrame[contour['y_min']:contour['y_max'], contour['x_min']:contour['x_max']]
+                # tags = detector.detect(frameSegment, estimate_tag_pose=True, camera_params=camera_params.values(),
+                #                        tag_size=0.2)
+                tags = detector.detect(frameSegment, estimate_tag_pose=False, tag_size=0.2)
 
-    xinEdgeCfg.out.link(edgeDetector.inputConfig)
+                if len(tags) > 0:
+                    for tag in tags:
+                        points = tag.corners.astype(np.int32)
+                        # Shift points since this is a snapshot
+                        points[:, 0] += contour['x_min']
+                        points[:, 1] += contour['y_min']
+                        cv2.polylines(videoFrame, [points], True, (120, 120, 120), 3)
+                        textX = min(points[:, 0])
+                        textY = min(points[:, 1]) + 20
+                        cv2.putText(videoFrame, "tag_id: {}".format(tag.tag_id), (textX, textY), cv2.FONT_HERSHEY_TRIPLEX,
+                                    0.6, (255, 255, 255))
 
-# # OAK-D Lite @ 1080P
-# camera_params = {
-#     "fx": 0.00337,
-#     "fY": 0.00337,
-#     "cx": 960,
-#     "cy": 540
-# }
+                        # pose_x = tag.pose_t[0] * 39.3701
+                        # pose_y = tag.pose_t[1] * 39.3701
+                        # pose_z = tag.pose_t[2] * 39.3701
+                        # cv2.putText(videoFrame, "x: {}".format(pose_x), (textX, textY + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.6,
+                        #             (255, 255, 255))
+                        # cv2.putText(videoFrame, "y: {}".format(pose_y), (textX, textY + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.6,
+                        #             (255, 255, 255))
+                        # cv2.putText(videoFrame, "z: {}".format(pose_z), (textX, textY + 60), cv2.FONT_HERSHEY_TRIPLEX, 0.6,
+                        #             (255, 255, 255))
 
-# OAK-D Lite @ 480P
-camera_params = {
-    "fx": 0.00337,
-    "fY": 0.00337,
-    "cx": 960,
-    "cy": 540
-}
+                        # TODO: Shift pose estimation by snapshot shift?
 
-detector = Detector(families='tag36h11',
-                       nthreads=1,
-                       quad_decimate=1.0,
-                       quad_sigma=0.0,
-                       refine_edges=1,
-                       decode_sharpening=0.25,
-                       debug=0)
+                        # print(tag)
+                        positives += 1
 
-fps = FPSHandler()
+                count += 1
+                if count > 5 or positives > 2:
+                    break
 
-# Connect to device and start pipeline
-with dai.Device(pipeline) as device:
-    # Output/input queues
-    # edgeLeftQueue = device.getOutputQueue(edgeLeftStr, 8, False)
-    # edgeRightQueue = device.getOutputQueue(edgeRightStr, 8, False)
-    videoQueue = device.getOutputQueue(videoStr, 4, False)
-    edgeQueue = device.getOutputQueue(edgeStr, 4, False)
-    edgeCfgQueue = device.getInputQueue(edgeCfgStr)
+            # if len(squares) > 0:
+            #     cv2.drawContours(edgeFrame, squares, -1, color=(255, 255, 255), thickness=cv2.FILLED)
 
-    print("Switch between sobel filter kernels using keys '1' and '2'")
+            fps.nextIter()
+            cv2.putText(videoFrame, "{:.2f}".format(fps.fps()), (0, 24), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255))
 
-    while(True):
-        # edgeLeft = edgeLeftQueue.get()
-        # edgeRight = edgeRightQueue.get()
-        videoOutput = videoQueue.get()
-        edgeOutput = edgeQueue.get()
+            # Show the frame
+            cv2.imshow(pipeline_info['videoQueue'], videoFrame)
 
-        videoFrame = videoOutput.getFrame()
-        edgeFrame = edgeOutput.getFrame()
-        # edgeRightFrame = edgeRight.getFrame()
-        # edgeRgbFrame = edgeRgb.getFrame()
-
-        thresh = cv2.threshold(edgeFrame, 25, 255, cv2.THRESH_BINARY)[1]
-        contours,_ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        squares = []
-        contourInfo = []
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            cnt_len = cv2.arcLength(cnt, True)
-            cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-
-            if len(cnt) == 4 and area > 1000:
-                squares.append(cnt)
-
-                # Pad the square and save the coordinates for later
-                rect = cv2.minAreaRect(cnt)
-                points = cv2.boxPoints(rect)
-                xmin = min(x for x in points[:, 0])
-                xmax = max(x for x in points[:, 0])
-                ymin = min(y for y in points[:, 1])
-                ymax = max(y for y in points[:, 1])
-                xmin = max(int(xmin - xmin * constants.PADDING_PERCENTAGE), 0)
-                xmax = min(int(xmax + xmin * constants.PADDING_PERCENTAGE), 1920)
-                ymin = max(int(ymin - ymin * constants.PADDING_PERCENTAGE), 0)
-                ymax = min(int(ymax + ymax * constants.PADDING_PERCENTAGE), 1080)
-
-                contourData = {'Contour': cnt,
-                               'x_min': xmin,
-                               'x_max': xmax,
-                               'y_min': ymin,
-                               'y_max': ymax,
-                               'area': area}
-
-                contourInfo.append(contourData)
-
-        contourInfo = sorted(contourInfo, key=lambda d: d['area'], reverse=True)
-        count = 0
-        positives = 0
-        for contour in contourInfo:
-            frameSegment = videoFrame[contour['y_min']:contour['y_max'], contour['x_min']:contour['x_max']]
-            tags = detector.detect(frameSegment, estimate_tag_pose=True, camera_params=camera_params.values(), tag_size=0.2)
-
-            if len(tags) > 0:
-                for tag in tags:
-                    points = tag.corners.astype(np.int32)
-                    # Shift points since this is a snapshot
-                    points[:, 0] += contour['x_min']
-                    points[:, 1] += contour['y_min']
-                    cv2.polylines(videoFrame, [points], True, (120, 120, 120), 3)
-                    textX = min(points[:, 0])
-                    textY = min(points[:, 1]) + 30
-                    cv2.putText(videoFrame, "tag_id: {}".format(tag.tag_id), (textX, textY), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255))
-
-                    # TODO: Shift pose estimation by snapshot shift
-
-                    print(tag)
-                    positives += 1
-
-            count += 1
-            if count > 5 or positives > 2:
+            key = cv2.waitKey(1)
+            if key == ord('q'):
                 break
 
-        # if len(squares) > 0:
-        #     cv2.drawContours(edgeFrame, squares, -1, color=(255, 255, 255), thickness=cv2.FILLED)
+            if key == ord('1'):
+                print("Switching sobel filter kernel.")
+                cfg = dai.EdgeDetectorConfig()
+                sobelHorizontalKernel = [[1, 0, -1], [2, 0, -2], [1, 0, -1]]
+                sobelVerticalKernel = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
+                cfg.setSobelFilterKernels(sobelHorizontalKernel, sobelVerticalKernel)
+                edgeCfgQueue.send(cfg)
 
-        fps.nextIter()
-        cv2.putText(videoFrame, "{:.2f}".format(fps.fps()), (0, 24), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255))
+            if key == ord('2'):
+                print("Switching sobel filter kernel.")
+                cfg = dai.EdgeDetectorConfig()
+                sobelHorizontalKernel = [[3, 0, -3], [10, 0, -10], [3, 0, -3]]
+                sobelVerticalKernel = [[3, 10, 3], [0, 0, 0], [-3, -10, -3]]
+                cfg.setSobelFilterKernels(sobelHorizontalKernel, sobelVerticalKernel)
+                edgeCfgQueue.send(cfg)
 
-        # Show the frame
-        cv2.imshow("mono", videoFrame)
-        # cv2.imshow(edgeRightStr, edgeRightFrame)
-        # cv2.imshow(edgeRgbStr, edgeRgbFrame)
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-
-        if key == ord('1'):
-            print("Switching sobel filter kernel.")
-            cfg = dai.EdgeDetectorConfig()
-            sobelHorizontalKernel = [[1, 0, -1], [2, 0, -2], [1, 0, -1]]
-            sobelVerticalKernel = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
-            cfg.setSobelFilterKernels(sobelHorizontalKernel, sobelVerticalKernel)
-            edgeCfgQueue.send(cfg)
-
-        if key == ord('2'):
-            print("Switching sobel filter kernel.")
-            cfg = dai.EdgeDetectorConfig()
-            sobelHorizontalKernel = [[3, 0, -3], [10, 0, -10], [3, 0, -3]]
-            sobelVerticalKernel = [[3, 10, 3], [0, 0, 0], [-3, -10, -3]]
-            cfg.setSobelFilterKernels(sobelHorizontalKernel, sobelVerticalKernel)
-            edgeCfgQueue.send(cfg)
+if __name__ == '__main__':
+    main()
