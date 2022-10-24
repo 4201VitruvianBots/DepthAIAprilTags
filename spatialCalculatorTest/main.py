22ab28001ccd7cda74d21433150b34d3c48f1295
Jonathan Dao <jonathandao0@gmail.com>
2022-10-23 17:54:22 -0700

    Code Cleanup

--- spatialCalculatorTest/main.py
+++ spatialCalculatorTest/main.py
@@ -354,15 +354,17 @@ def main():
                 testGui.updateFrames(frameRight, depthFrameColor)
             else:
                 latencyStd = np.std(latency) if len(latency) < 100 else np.std(latency[-100:])
-                print('Latency: {:.2f} ms, Std: {:.2f}'.format(avgLatency, np.std(latencyStd)))
-                print("FPS: {:.2f}".format(fps.fps()))
+                print('FPS: {:.2f}\tLatency: {:.2f} ms\tStd: {:.2f}'.format(fps.fps(), avgLatency, np.std(latencyStd)))
 
             key = cv2.waitKey(1)
             if key == ord('q'):
                 break
             elif key == ord(' '):
                 if 'gyro' in locals():
-                    gyro.reset()
+                    gyro.resetAll()
+
+            if not testGui.isVisible():
+                break
 
 
 class DebugWindow(QtWidgets.QWidget):
@@ -398,7 +400,7 @@ class DebugWindow(QtWidgets.QWidget):
     def updateFrames(self, monoFrame, depthFrame):
         activeTab = self.frameWidget.currentIndex()
 
-        if activeTab == 0:
+        if activeTab == 0 or activeTab == 2:
             monoFrame = cv2.cvtColor(monoFrame, cv2.COLOR_GRAY2RGB)
             img = QtGui.QImage(monoFrame, monoFrame.shape[1], monoFrame.shape[0], QtGui.QImage.Format_RGB888)
             pix = QtGui.QPixmap.fromImage(img)
@@ -407,7 +409,7 @@ class DebugWindow(QtWidgets.QWidget):
             self.monoFrame.setMaximumWidth(monoFrame.shape[1])
             self.monoFrame.setMaximumHeight(monoFrame.shape[0])
             self.monoFrame.setPixmap(pix)
-        elif activeTab == 1:
+        elif activeTab == 1 or activeTab == 2:
             depthFrame = cv2.cvtColor(depthFrame, cv2.COLOR_BGR2RGB)
             img = QtGui.QImage(depthFrame, depthFrame.shape[1], depthFrame.shape[0], QtGui.QImage.Format_RGB888)
             pix = QtGui.QPixmap.fromImage(img)
@@ -416,24 +418,6 @@ class DebugWindow(QtWidgets.QWidget):
             self.depthFrame.setMaximumWidth(depthFrame.shape[1])
             self.depthFrame.setMaximumHeight(depthFrame.shape[0])
             self.depthFrame.setPixmap(pix)
-        elif activeTab == 2:
-            monoFrame = cv2.cvtColor(monoFrame, cv2.COLOR_GRAY2RGB)
-            img = QtGui.QImage(monoFrame, monoFrame.shape[1], monoFrame.shape[0], QtGui.QImage.Format_RGB888)
-            pix = QtGui.QPixmap.fromImage(img)
-            self.monoFrame2.setMinimumWidth(monoFrame.shape[1])
-            self.monoFrame2.setMinimumHeight(monoFrame.shape[0])
-            self.monoFrame2.setMaximumWidth(monoFrame.shape[1])
-            self.monoFrame2.setMaximumHeight(monoFrame.shape[0])
-            self.monoFrame2.setPixmap(pix)
-
-            depthFrame = cv2.cvtColor(depthFrame, cv2.COLOR_BGR2RGB)
-            img = QtGui.QImage(depthFrame, depthFrame.shape[1], depthFrame.shape[0], QtGui.QImage.Format_RGB888)
-            pix = QtGui.QPixmap.fromImage(img)
-            self.depthFrame2.setMinimumWidth(depthFrame.shape[1])
-            self.depthFrame2.setMinimumHeight(depthFrame.shape[0])
-            self.depthFrame2.setMaximumWidth(depthFrame.shape[1])
-            self.depthFrame2.setMaximumHeight(depthFrame.shape[0])
-            self.depthFrame2.setPixmap(pix)
 
     def updateTagFilter(self):
         self.tagFilter = np.array(np.where([self.tagCheckbox0.isChecked(), self.tagCheckbox1.isChecked(),
